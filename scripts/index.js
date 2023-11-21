bafaee4ae8e3d795ce02ffc25c25e4105f68c4d5
chathamabate <chathamabate@gmail.com>
2023-11-21 11:02:38 -0500

    Horizontal Styles

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,12 @@
 const GITHUB_URL = "https://chathamabate.github.io/"
 
 async function main() {
+    // Hacky resize trick.
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
     let relProjectPaths = [
         "data/TIUnit.json",
